1ed454190538da65fc361b3f583a9b5a06047b41
Neil N. Carlson <neil.n.carlson@gmail.com>
2026-03-01 09:53:12 -0700

    Add Sphinx documentation build

---
+++ doc/sphinx/conf.py
@@ -0,0 +1,11 @@
+project = "Index Map"
+copyright = "2022, 2026 Neil N. Carlson"
+author = "Neil N. Carlson"
+
+extensions = []
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
---
+++ doc/sphinx/index.rst
@@ -0,0 +1,9 @@
+Index Map
+=========
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents
+
+   index_map_type
+
